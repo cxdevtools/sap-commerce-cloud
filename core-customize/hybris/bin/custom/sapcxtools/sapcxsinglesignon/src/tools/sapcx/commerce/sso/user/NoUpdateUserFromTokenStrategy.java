8b263785594fee0a9771c5f46b64b0f5cdbafed0
Stefan Bechtold <bechte@users.noreply.github.com>
2025-12-16 14:17:04 +0100

    remove extensions no longer needed with JDK21

--- core-customize/hybris/bin/custom/sapcxtools/sapcxsinglesignon/src/tools/sapcx/commerce/sso/user/NoUpdateUserFromTokenStrategy.java
+++
@@ -1,9 +0,0 @@
-package tools.sapcx.commerce.sso.user;
-
-import org.springframework.security.oauth2.jwt.Jwt;
-
-public class NoUpdateUserFromTokenStrategy implements UpdateUserFromTokenStrategy {
-	@Override
-	public void updateUserFromToken(String userId, Jwt token) {
-	}
-}
